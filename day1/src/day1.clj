6c14ff2ecb06a314aa049a0eb6214e2632b0d7ec
RJ Russell <RJ-Russell@users.noreply.github.com>
2020-03-21 15:30:04 -0600

    day1: complete

--- day1/src/day1.clj
+++ day1/src/day1.clj
@@ -1,1 +1,35 @@
-(ns day1)
+(ns day1
+  (:require [clojure.string :refer [split]]))
+
+(def parse-input
+  (->>
+   (split (slurp "resources/day1_data") #"\n")
+   (map #(Integer/parseInt %))))
+
+(defn mass->fuel [m]
+  (- (int (Math/floor (/ m 3))) 2))
+
+(defn fuel-per-module [initial]
+  (loop
+   [val (mass->fuel initial)
+    coll []]
+    (if (<= val 0)
+      coll
+      (recur (mass->fuel val) (conj coll val)))))
+
+(defn total-fuel-per-module [val]
+  (->> val
+       (fuel-per-module)
+       (reduce +)))
+
+(defn day1
+  []
+  (reduce + (map mass->fuel parse-input))
+  ;; => 3454026
+  )
+
+(defn day2
+  []
+  (reduce + (map total-fuel-per-module parse-input))
+  ;; => 5178170
+  )
--- day1/test/day1_test.clj
+++ day1/test/day1_test.clj
@@ -1,3 +1,20 @@
 (ns day1-test
-  (:require [clojure.test :refer :all]
-            [day1 :refer :all]))
+  (:require
+   [clojure.test :refer [deftest is]]
+   [day1 :as d1]))
+
+
+(deftest calculate-fuel-returns-correct-value-based-on-mass
+  (is (= 2 (d1/mass->fuel 12)))
+  (is (= 2 (d1/mass->fuel 14)))
+  (is (= 654 (d1/mass->fuel 1969)))
+  (is (= 33583 (d1/mass->fuel 100756))))
+
+(deftest returns-correct-list-of-fuel-subvalues
+  (is (= [2] (d1/fuel-per-module 14)))
+  (is (= [654 216 70 21 5] (d1/fuel-per-module 1969))))
+
+(deftest returns-correct-total-fuel-per-module
+  (is (= 2 (d1/total-fuel-per-module 14)))
+  (is (= 966 (d1/total-fuel-per-module 1969)))
+  (is (= 50346 (d1/total-fuel-per-module 100756))))
