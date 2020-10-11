fd1462197e14df34bf4fbc3c682d3687e05bddb1
RJ Russell <RJ-Russell@users.noreply.github.com>
2020-10-10 20:51:59 -0600

    reverse range calls for L and D

--- 2019-Clojure/src/day3.clj
+++ 2019-Clojure/src/day3.clj
@@ -13,6 +13,10 @@
         distance (Integer/parseInt (subs instruction 1))]
     (case direction
       \R (for [x' (range (inc x) (+ x distance 1))] [x' y])
-      \L (for [x' (range (- x distance) x)] [x' y])
+      \L (for [x' (reverse (range (- x distance) x))] [x' y])
       \U (for [y' (range (inc y) (+ y distance 1))] [x y'])
-      \D (for [y' (range (- y distance) y)] [x y']))))
+      \D (for [y' (reverse (range (- y distance) y))] [x y']))))
+
+(defn process-instructions
+  ([directions] (process-instructions directions [0 0]))
+  ([directions origin]))
--- 2019-Clojure/test/day3/day3_test.clj
+++ 2019-Clojure/test/day3/day3_test.clj
@@ -7,7 +7,7 @@
 (deftest process-single-instruction
   (testing "Move in a single direction a given number from a given origin"
     (is (=  [[1 1] [2 1] [3 1] [4 1]] (d3/process-instruction [0 1] "R4")))
-    (is (=  [[-4 1] [-3 1] [-2 1] [-1 1]] (d3/process-instruction [0 1] "L4")))
-    (is (=  [[0 1] [1 1] [2 1] [3 1] [4 1]] (d3/process-instruction [5 1] "L5")))
+    (is (=  [[-1 1] [-2 1] [-3 1] [-4 1]] (d3/process-instruction [0 1] "L4")))
+    (is (=  [[4 1] [3 1] [2 1] [1 1] [0 1]] (d3/process-instruction [5 1] "L5")))
     (is (=  [[1 2] [1 3] [1 4] [1 5]] (d3/process-instruction [1 1] "U4")))
-    (is (=  [[0 -4] [0 -3] [0 -2] [0 -1]] (d3/process-instruction [0 0] "D4")))))
+    (is (=  [[0 -1] [0 -2] [0 -3] [0 -4]] (d3/process-instruction [0 0] "D4")))))
