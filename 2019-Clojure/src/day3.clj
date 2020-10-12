1df63c486149c8f6ee8dd386b4a2396a400455f5
RJ Russell <RJ-Russell@users.noreply.github.com>
2020-10-11 18:04:06 -0600

    part1 complete, part2 having issues

--- 2019-Clojure/src/day3.clj
+++ 2019-Clojure/src/day3.clj
@@ -1,5 +1,6 @@
 (ns day3
-  (:require [clojure.string :refer [split]]))
+  (:require [clojure.string :refer [split split-lines]]
+            [clojure.set :refer [intersection]]))
 
 (def parse-input
   (->
@@ -12,11 +13,56 @@
   (let [direction (first instruction)
         distance (Integer/parseInt (subs instruction 1))]
     (case direction
-      \R (for [x' (range (inc x) (+ x distance 1))] [x' y])
-      \L (for [x' (reverse (range (- x distance) x))] [x' y])
-      \U (for [y' (range (inc y) (+ y distance 1))] [x y'])
-      \D (for [y' (reverse (range (- y distance) y))] [x y']))))
+      \R (for [x' (reverse (range (inc x) (+ x distance 1)))] [x' y])
+      \L (for [x' (range (- x distance) x)] [x' y])
+      \U (for [y' (reverse (range (inc y) (+ y distance 1)))] [x y'])
+      \D (for [y' (range (- y distance) y)] [x y']))))
 
 (defn process-instructions
-  ([directions] (process-instructions directions [0 0]))
-  ([directions origin]))
+  ([instructions] (process-instructions [0 0] instructions))
+  ([origin instructions]
+   (loop [origin origin
+          [fi & ri] instructions
+          result []]
+     (if (nil? fi)
+       (apply concat result)
+       (let [segment (process-instruction origin fi)
+             new-origin (first segment)]
+         (recur new-origin ri (conj result segment)))))))
+
+(defn manhattan-distance
+  ([[x y]]
+   (Math/abs (+ x y))))
+
+(defn get-wires
+  [instructions]
+  (-> instructions
+      (split #",")
+      (process-instructions)))
+
+(defn part1
+  []
+  (let [input parse-input
+        wire1 (get-wires (first input))
+        wire2 (get-wires (second input))]
+    (apply min (map manhattan-distance (intersection (set wire1) (set wire2))))))
+  ;; => 248
+
+
+(defn find-thing [needle haystack]
+  (first (keep-indexed #(when (= %2 needle) %1) haystack)))
+
+(defn part2
+  []
+  (let [input parse-input
+        wire1 (get-wires (first input))
+        wire2 (get-wires (second input))
+        intersections (intersection (set wire1) (set wire2))]
+    (+ 2
+       (apply min (for [point intersections]
+                    (+ (find-thing point wire1)
+                       (find-thing point wire2)))))))
+  ;; => 28938
+
+
+;; 28580
--- 2019-Clojure/test/day3/day3_test.clj
+++ 2019-Clojure/test/day3/day3_test.clj
@@ -5,9 +5,23 @@
 
 
 (deftest process-single-instruction
-  (testing "Move in a single direction a given number from a given origin"
-    (is (=  [[1 1] [2 1] [3 1] [4 1]] (d3/process-instruction [0 1] "R4")))
-    (is (=  [[-1 1] [-2 1] [-3 1] [-4 1]] (d3/process-instruction [0 1] "L4")))
-    (is (=  [[4 1] [3 1] [2 1] [1 1] [0 1]] (d3/process-instruction [5 1] "L5")))
-    (is (=  [[1 2] [1 3] [1 4] [1 5]] (d3/process-instruction [1 1] "U4")))
-    (is (=  [[0 -1] [0 -2] [0 -3] [0 -4]] (d3/process-instruction [0 0] "D4")))))
+  (testing "Move in a single direction a given number from a given origin returns final end point"
+    (is (=  [[4 1] [3 1] [2 1] [1 1]] (d3/process-instruction [0 1] "R4")))
+    (is (=  [[-4 1] [-3 1] [-2 1] [-1 1]] (d3/process-instruction [0 1] "L4")))
+    (is (=  [[0 1] [1 1] [2 1] [3 1] [4 1]] (d3/process-instruction [5 1] "L5")))
+    (is (=  [[1 5] [1 4] [1 3] [1 2]] (d3/process-instruction [1 1] "U4")))
+    (is (=  [[0 -4] [0 -3] [0 -2] [0 -1]] (d3/process-instruction [0 0] "D4")))))
+
+(deftest process-series-of-instructions
+  (testing "Given a series of instructions, creates a cumulative list of endpoints"
+    (is (= [[1 0] [1 -3] [1 -2] [1 -1]] (d3/process-instructions [0 0] ["R1" "D3"])))
+    (is (= [[0 7] [0 6] [0 5] [0 4] [0 3] [0 2] [0 1]
+            [6 7] [5 7] [4 7] [3 7] [2 7] [1 7]
+            [6 3] [6 4] [6 5] [6 6]
+            [2 3] [3 3] [4 3] [5 3]]
+           (d3/process-instructions [0 0] ["U7" "R6" "D4" "L4"])))
+    (is (= [[8 0] [7 0] [6 0] [5 0] [4 0] [3 0] [2 0] [1 0]
+            [8 5] [8 4] [8 3] [8 2] [8 1]
+            [3 5] [4 5] [5 5] [6 5] [7 5]
+            [3 2] [3 3] [3 4]]
+           (d3/process-instructions [0 0] ["R8" "U5" "L5" "D3"])))))
